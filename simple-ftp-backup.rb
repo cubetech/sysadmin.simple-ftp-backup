de3c84b722404c37f3949d8daa2c94a754c4bdff
Christoph S. Ackermann <info@acki.be>
2013-02-13 19:21:14 +0100

    hard work on main script, some prints and comments

--- simple-ftp-backup.rb
+++ simple-ftp-backup.rb
@@ -7,7 +7,9 @@ require 'net/ftp'
 require 'settings'
 require 'rubygems'
 require 'sequel'
+require 'date'
 
+# Function for creating recursive directories
 class Dir
   def self.mkdirs(p)
      return if File.exists?(p)
@@ -17,24 +19,44 @@ class Dir
   end
 end
 
+# FTP function to remove a nonempty folder
+def ftp_remove_all(ftp,file)    
+  begin
+    ftp.chdir(file)
+    ftp.chdir("../")      
+    data = ftp.list('-A ' + file)      
+    for line in data        
+      filename = line.split(/(\s)/)        
+      ftp_remove_all(ftp,file + "/" + filename[filename.length-1])        
+    end            
+    ftp.rmdir(file)
+  rescue      
+    ftp.delete(file)
+  end     
+end
+
 # Initial setup
 timestamp = Time.now.strftime("%Y%m%d-%H%M")
 full_tmp_path = File.join(TMP_BACKUP_PATH, "simple-s3-backup-" << timestamp)
 if defined?(DATEPATH) and DATEPATH!=false
   basepath = "#{FTP_BASEPATH}/#{FTP_FOLDER}/#{timestamp}"
+  clean_basepath = "/#{FTP_BASEPATH}/#{FTP_FOLDER}"
 elsif defined?(FTP_BASEPATH) and FTP_BASEPATH!=nil and FTP_BASEPATH!=""
   basepath = "#{FTP_BASEPATH}/#{FTP_FOLDER}"
+  clean_basepath = "/#{FTP_BASEPATH}/#{FTP_FOLDER}"
 else
   basepath = FTP_FOLDER
+  clean_basepath = "/#{FTP_FOLDER}"
 end
 
 # Remove double slash
 basepath = basepath.squeeze('/').strip
 
-# Find/create the backup bucket
+# Connect to FTP
 ftp = Net::FTP.new(FTP_HOST)
 ftp.login(FTP_USER, FTP_PASS)
 
+# Find/create the backup bucket
 flist = basepath.split("/")
 flist.each do |folder|
   folderlist = ftp.list()
@@ -42,11 +64,14 @@ flist.each do |folder|
   ftp.chdir(folder)
 end
 
+print "\nConnected to FTP and selected bucket\n\n"
+
 # Create tmp directory
 Dir.mkdirs full_tmp_path
 
 # Perform MySQL backup of all databases or specific ones
 if defined?(MYSQL_ALL or MYSQL_DBS)
+
   # Build an array of databases to backup
   if defined?(MYSQL_ALL)
     connection = Sequel.mysql nil, :user => MYSQL_USER, :password => MYSQL_PASS, :host => 'localhost', :encoding => 'utf8'
@@ -54,56 +79,88 @@ if defined?(MYSQL_ALL or MYSQL_DBS)
   elsif defined?(MYSQL_DBS)
     @databases = MYSQL_DBS
   end
+
   # Fail if there are no databases to backup
   raise "Error: There are no db's to backup." if @databases.empty?
 
   @databases.each do |db|
+
+    # Define file name
     db_filename = "db-#{db}-#{timestamp}.sql.gz"
+
+    # Define password parameter
     if defined?(MYSQL_PASS) and MYSQL_PASS!=nil and MYSQL_PASS!=""
       password_param = "-p#{MYSQL_PASS}"
     else
       password_param = ""
     end
+
     # Perform the mysqldump and compress the output to file
     system("#{MYSQLDUMP_CMD} -u #{MYSQL_USER} #{password_param} --single-transaction --add-drop-table --add-locks --create-options --disable-keys --extended-insert --quick #{db} | #{GZIP_CMD} -#{GZIP_STRENGTH} -c > #{full_tmp_path}/#{db_filename}")
+
     # Upload file to FTP
     folderlist = ftp.list()
     if !folderlist.any?{|dir| dir.match(/\s#{MYSQLPATH}$/)}
       ftp.mkdir(MYSQLPATH)
     end
     ftp.putbinaryfile("#{full_tmp_path}/#{db_filename}", "#{MYSQLPATH}/#{db_filename}")
+
   end
+  print "MySQL backup finished\n"
 end
 
 
 # Perform MongoDB backups
 if defined?(MONGO_DBS)
+
+  # Create dump dir
   mdb_dump_dir = File.join(full_tmp_path, "mdbs")
   Dir.mkdirs(mdb_dump_dir)
+
+  # Create dumps and upload them to ftp
   MONGO_DBS.each do |mdb|
+
+    # Create dump and archive
     mdb_filename = "mdb-#{mdb}.tgz"
     system("#{MONGODUMP_CMD} -h #{MONGO_HOST} -d #{mdb} -o #{mdb_dump_dir} && cd #{mdb_dump_dir}/#{mdb} && #{TAR_CMD} -czf #{full_tmp_path}/#{mdb_filename} .")
-    S3Object.store("mongodb/#{timestamp}/#{mdb_filename}", open("#{full_tmp_path}/#{mdb_filename}"), S3_BUCKET)
+
+    # Upload file to FTP
+    folderlist = ftp.list()
+    if !folderlist.any?{|dir| dir.match(/\s#{MONGOPATH}$/)}
+      ftp.mkdir(MONGOPATH)
+    end
+    ftp.putbinaryfile("#{full_tmp_path}/#{mdb_filename}", "#{MONGOPATH}/#{mdb_filename}")
+
   end
+  print "MongoDB backup finished\n"
   system("rm -rf #{mdb_dump_dir}")
 end
 
 # Perform directory backups
 if defined?(DIRECTORIES)
   DIRECTORIES.each do |name, dir|
+
+    # Define file name
     dir_filename = "dir-#{name}.tgz"
+
+    # Get excludes
     excludes = ""
     if defined?(DIRECTORIES_EXCLUDE)
       DIRECTORIES_EXCLUDE.each do |de|
         excludes += "--exclude=\"#{de}\" "
       end
     end
+
+    # Create archive
     system("cd #{dir} && #{TAR_CMD} #{excludes} -czf #{full_tmp_path}/#{dir_filename} .")
-    # Split and upload file to FTP
+    
+    # Create file path on server if needed
     folderlist = ftp.list()
     if !folderlist.any?{|dir| dir.match(/\s#{FILEPATH}$/)}
       ftp.mkdir(FILEPATH)
     end
+
+    # Split and upload file to FTP
     filesize = File.size("#{full_tmp_path}/#{dir_filename}").to_f / 1024000
     if filesize > SPLIT_SIZE
       system("split -d -b #{SPLIT_SIZE}m #{full_tmp_path}/#{dir_filename} #{full_tmp_path}/#{dir_filename}.")
@@ -116,6 +173,7 @@ if defined?(DIRECTORIES)
       ftp.putbinaryfile("#{full_tmp_path}/#{dir_filename}", "#{FILEPATH}/#{dir_filename}")
     end
   end
+  print "Directories backup finished\n"
 end
 
 # Perform single files backups
@@ -124,28 +182,55 @@ if defined?(SINGLE_FILES)
 
     # Create a directory to collect the files
     files_tmp_path = File.join(full_tmp_path, "#{name}-tmp")
-    FileUtils.mkdir_p files_tmp_path
+    Dir.mkdirs(files_tmp_path)
 
     # Filename for files
     files_filename = "files-#{name}.tgz"
 
     # Copy files to temp directory
-    FileUtils.cp files, files_tmp_path
+    files.each do |file|
+      system("cp #{file} #{files_tmp_path}")
+    end
+
+    # Check if FTP dir exists
+    folderlist = ftp.list()
+    if !folderlist.any?{|dir| dir.match(/\s#{FILEPATH}$/)}
+      ftp.mkdir(FILEPATH)
+    end
 
     # Create archive & copy to S3
-    system("cd #{files_tmp_path} && #{TAR_CMD} -czf #{full_tmp_path}/#{files_filename} .")
-    S3Object.store("files/#{timestamp}/#{files_filename}", open("#{full_tmp_path}/#{files_filename}"), S3_BUCKET)
+    system("cd #{files_tmp_path} && #{TAR_CMD} -czf #{full_tmp_path}/#{files_filename} *")
+    ftp.putbinaryfile("#{full_tmp_path}/#{files_filename}", "#{FILEPATH}/#{files_filename}")
 
     # Remove the temporary directory for the files
-    FileUtils.remove_dir files_tmp_path
+    system("rm -rf #{files_tmp_path}")
   end
+  print "File backup finished\n"
 end
 
 # Remove tmp directory
 system("rm -rf #{full_tmp_path}")
 
+print "\n"
+
 # Now, clean up unwanted archives
-#cutoff_date = Time.now.strftime("%Y%m%d-") - (DAYS_OF_ARCHIVES * 86400)
-#bucket.objects.select{ |o| o.last_modified.to_i < cutoff_date }.each do |f|
-#  S3Object.delete(f.key, S3_BUCKET)
-#end
+list = ftp.list("#{clean_basepath}")
+list.each do |file|
+
+  # Get file name
+  file = file.split(/\s+/).last
+  
+  # Get dates
+  date = DateTime.strptime(file, "%Y%m%d-%H%M")
+  limit = DateTime.now - DAYS_OF_ARCHIVES
+  
+  # Check if deletion is needed, if yes, delete
+  if date < limit
+    ftp_remove_all(ftp, "#{clean_basepath}/#{file}")
+    print "Old backup #{file} deleted.\n"
+  end
+end
+
+print "\nBackup finished.\n"
+
+exit
