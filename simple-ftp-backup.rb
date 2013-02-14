2f3e6c33126c22c8ebdb2a245965ca91dad015ff
Christoph S. Ackermann <christoph.ackermann@cubetech.ch>
2013-02-14 11:10:09 +0100

    some improvements for prevent ftp timeout

--- simple-ftp-backup.rb
+++ simple-ftp-backup.rb
@@ -35,6 +35,22 @@ def ftp_remove_all(ftp,file)
   end     
 end
 
+def ftp_open
+  begin
+    ftp = Net::FTP.new(FTP_HOST)
+    ftp.login(FTP_USER, FTP_PASS)
+    return ftp
+  end
+end
+
+def ftp_close(ftp)
+  begin
+    path = ftp.pwd
+    ftp.close
+    return path
+  end
+end
+
 # Initial setup
 timestamp = Time.now.strftime("%Y%m%d-%H%M")
 full_tmp_path = File.join(TMP_BACKUP_PATH, "simple-s3-backup-" << timestamp)
@@ -52,17 +68,15 @@ end
 # Remove double slash
 basepath = basepath.squeeze('/').strip
 
-# Connect to FTP
-ftp = Net::FTP.new(FTP_HOST)
-ftp.login(FTP_USER, FTP_PASS)
-
 # Find/create the backup bucket
+ftp = ftp_open()
 flist = basepath.split("/")
 flist.each do |folder|
   folderlist = ftp.list()
   ftp.mkdir(folder) if !folderlist.any?{|dir| dir.match(/\s#{folder}$/)}
   ftp.chdir(folder)
 end
+path = ftp_close(ftp)
 
 print "\nConnected to FTP and selected bucket\n\n"
 
@@ -82,6 +96,9 @@ if defined?(MYSQL_ALL or MYSQL_DBS)
 
   # Fail if there are no databases to backup
   raise "Error: There are no db's to backup." if @databases.empty?
+
+  ftp = ftp_open
+  ftp.chdir(path)
 
   @databases.each do |db|
 
@@ -106,6 +123,7 @@ if defined?(MYSQL_ALL or MYSQL_DBS)
     ftp.putbinaryfile("#{full_tmp_path}/#{db_filename}", "#{MYSQLPATH}/#{db_filename}")
 
   end
+  path = ftp_close(ftp)
   print "MySQL backup finished\n"
 end
 
@@ -117,6 +135,8 @@ if defined?(MONGO_DBS)
   mdb_dump_dir = File.join(full_tmp_path, "mdbs")
   Dir.mkdirs(mdb_dump_dir)
 
+  ftp = ftp_open
+  ftp.chdir(path)
   # Create dumps and upload them to ftp
   MONGO_DBS.each do |mdb|
 
@@ -132,6 +152,7 @@ if defined?(MONGO_DBS)
     ftp.putbinaryfile("#{full_tmp_path}/#{mdb_filename}", "#{MONGOPATH}/#{mdb_filename}")
 
   end
+  path = ftp_close(ftp)
   print "MongoDB backup finished\n"
   system("rm -rf #{mdb_dump_dir}")
 end
@@ -155,10 +176,13 @@ if defined?(DIRECTORIES)
     system("cd #{dir} && #{TAR_CMD} #{excludes} -czf #{full_tmp_path}/#{dir_filename} .")
     
     # Create file path on server if needed
+    ftp = ftp_open
+    ftp.chdir(path)
     folderlist = ftp.list()
     if !folderlist.any?{|dir| dir.match(/\s#{FILEPATH}$/)}
       ftp.mkdir(FILEPATH)
     end
+    path = ftp_close(ftp)
 
     # Split and upload file to FTP
     filesize = File.size("#{full_tmp_path}/#{dir_filename}").to_f / 1024000
@@ -167,10 +191,16 @@ if defined?(DIRECTORIES)
       system("rm -rf #{full_tmp_path}/#{dir_filename}")
       Dir.glob("#{full_tmp_path}/#{dir_filename}.*") do |item|
         basename = File.basename(item)
+        ftp = ftp_open
+        ftp.chdir(path)
         ftp.putbinaryfile("#{item}", "#{FILEPATH}/#{basename}")
+        path = ftp_close(ftp)
       end
     else
+      ftp = ftp_open
+      ftp.chdir(path)
       ftp.putbinaryfile("#{full_tmp_path}/#{dir_filename}", "#{FILEPATH}/#{dir_filename}")
+      path = ftp_close(ftp)
     end
   end
   print "Directories backup finished\n"
@@ -193,14 +223,20 @@ if defined?(SINGLE_FILES)
     end
 
     # Check if FTP dir exists
+    ftp = ftp_open
+    ftp.chdir(path)
     folderlist = ftp.list()
     if !folderlist.any?{|dir| dir.match(/\s#{FILEPATH}$/)}
       ftp.mkdir(FILEPATH)
     end
+    path = ftp_close(ftp)
 
     # Create archive & copy to S3
     system("cd #{files_tmp_path} && #{TAR_CMD} -czf #{full_tmp_path}/#{files_filename} *")
+    ftp = ftp_open
+    ftp.chdir(path)
     ftp.putbinaryfile("#{full_tmp_path}/#{files_filename}", "#{FILEPATH}/#{files_filename}")
+    path = ftp_close(ftp)
 
     # Remove the temporary directory for the files
     system("rm -rf #{files_tmp_path}")
@@ -214,6 +250,7 @@ system("rm -rf #{full_tmp_path}")
 print "\n"
 
 # Now, clean up unwanted archives
+ftp = ftp_open
 list = ftp.list("#{clean_basepath}")
 list.each do |file|
 
@@ -230,6 +267,7 @@ list.each do |file|
     print "Old backup #{file} deleted.\n"
   end
 end
+ftp_close(ftp)
 
 print "\nBackup finished.\n"
 
